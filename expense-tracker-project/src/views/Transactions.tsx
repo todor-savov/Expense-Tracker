3216190d0a6656b5d5867d2531fbf3148df04324
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2024-07-05 18:05:12 +0300

    Add Transactions view to load when "/transactions" route is matched

---
+++ expense-tracker-project/src/views/Transactions.tsx
@@ -0,0 +1,13 @@
+import AllTransactions from "../components/AllTransactions/AllTransactions.tsx";
+
+const Transactions = () => {
+
+    return (
+        <>
+            <AllTransactions />
+
+
+        </>
+    )
+}
+export default Transactions;

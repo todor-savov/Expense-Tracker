30ff771fc71cab2a5fa198fb5873fa8f540d4eca
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2024-07-05 18:03:27 +0300

    Update the AddTransaction component to remove animation from icons and make the upload form visible always

--- expense-tracker-project/src/components/AddTransaction/AddTransaction.tsx
+++ expense-tracker-project/src/components/AddTransaction/AddTransaction.tsx
@@ -1,10 +1,9 @@
-import { useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCalendarDays, faTags, faCalculator, faList, faCreditCard, faReceipt } from '@fortawesome/free-solid-svg-icons';
+import { faCalendarDays, faTags, faCalculator, faList, faCreditCard } from '@fortawesome/free-solid-svg-icons';
+import UploadReceipt from '../UploadReceipt/UploadReceipt';
 import './AddTransaction.css';
 
 const AddTransaction = () => {
-    const navigate = useNavigate();
 
     return (
         <>  
@@ -12,18 +11,18 @@ const AddTransaction = () => {
                 <h2>Expense Information</h2>
 
                 <div className='expense-form-row-1'>
-                    <FontAwesomeIcon icon={faCalendarDays} beat size="xl" style={{color: "#74C0FC",}} />
+                    <FontAwesomeIcon icon={faCalendarDays} size="xl" style={{color: "#74C0FC",}} />
                     <input id="expense-date" type="date" name="expense-date" required/>
 
-                    <FontAwesomeIcon icon={faTags} fade size="xl" style={{color: "#74C0FC",}} />
+                    <FontAwesomeIcon icon={faTags} size="xl" style={{color: "#74C0FC",}} />
                     <input id="expense-name" type="text" name="expense-name" placeholder='Expense Name' required/>
 
-                    <FontAwesomeIcon icon={faCalculator} bounce size="xl" style={{color: "#74C0FC",}} />
+                    <FontAwesomeIcon icon={faCalculator} size="xl" style={{color: "#74C0FC",}} />
                     <input id="expense-amount" type="text" name="expense-amount" placeholder='Expense Amount' required/>
                 </div>
 
                 <div className='expense-form-row-2'>
-                    <FontAwesomeIcon icon={faList} fade size="xl" style={{color: "#74C0FC",}} />
+                    <FontAwesomeIcon icon={faList} size="xl" style={{color: "#74C0FC",}} />
                     <select id="expense-category" name="expense-category" required>
                         <option value="select">Category</option>
                         <option value="food">Food</option>
@@ -33,15 +32,16 @@ const AddTransaction = () => {
                         <option value="others">Others</option>
                     </select>
 
-                    <FontAwesomeIcon icon={faCreditCard} bounce size="xl" style={{color: "#74C0FC",}} />
+                    <FontAwesomeIcon icon={faCreditCard} size="xl" style={{color: "#74C0FC",}} />
                     <select id="expense-payment" name="expense-payment" required>
                         <option value="select">Payment Method</option>
                         <option value="cash">Cash</option>
                         <option value="card">Card</option>
                     </select>
+                </div>
 
-                    <FontAwesomeIcon icon={faReceipt} beat size="xl" style={{color: "#74C0FC",}} />
-                    <button id='upload-sales-receipt' onClick={() => navigate('/upload-receipt')}>Receipt</button>
+                <div className='expense-form-row-3'>
+                    <UploadReceipt />
                 </div>
                 
                 <button type="submit" id='add-expense'>Add Expense</button>

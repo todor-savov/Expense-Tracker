ea529b0334a7ac1df41025762a11b1fffa72d28d
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2024-07-05 16:15:01 +0300

    Update AuthContext.tsx to comply with TypeScript requirements

--- expense-tracker-project/src/context/AuthContext.tsx
+++ expense-tracker-project/src/context/AuthContext.tsx
@@ -1,5 +1,8 @@
 import { createContext } from "react";
 
-const AuthContext = createContext(null);
+const AuthContext = createContext({  
+    isLoggedIn: {status: false, user: ''},
+    setLoginState: (value: {status: boolean, user: string}) => {}
+});
     
 export default AuthContext;

d723a1c50a8295760749cdc2d625bc0fc89ce4a0
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2024-07-06 16:32:15 +0300

    Add typization for props in UploadReceipt component as well as state to return the URL of the uploaded file

--- expense-tracker-project/src/components/UploadReceipt/UploadReceipt.tsx
+++ expense-tracker-project/src/components/UploadReceipt/UploadReceipt.tsx
@@ -4,17 +4,21 @@ import { faReceipt } from '@fortawesome/free-solid-svg-icons';
 import { uploadFile } from "../../service/storage-service.ts";
 import './UploadReceipt.css';
 
-const UploadReceipt = () => {
+interface UploadReceiptProps {
+    addReceipt: (url: string) => void;
+  }
+
+const UploadReceipt: React.FC<UploadReceiptProps> = ({ addReceipt }) => {
     const fileInputRef = useRef<HTMLInputElement>(null);
     const [fileToUpload, setFileToUpload] = useState<File | null>(null);
+    const [receiptURL, setReceiptURL] = useState<string | null>(null);
 
     useEffect(() => {
         const uploadHandler = async () => {
             const receiptURL = await uploadFile(fileToUpload as File);
-            console.log(receiptURL);
-
+            setReceiptURL(receiptURL);
+            addReceipt(receiptURL);
         }
-
         if (fileToUpload) uploadHandler();
     }, [fileToUpload]);
 
@@ -25,17 +29,18 @@ const UploadReceipt = () => {
     }
 
     return (
-       
-
         <div className='upload-receipt'>
-            
             <FontAwesomeIcon icon={faReceipt} size="2xl" style={{color: "#74C0FC",}} />
 
-
             <input type="file" id="file" name="file" accept="image/*" ref={fileInputRef} onChange={(event) => handleUpload(event)} />
-            <label htmlFor="file">Upload Sales Receipt</label>
+            <label htmlFor="file">Sales Receipt</label>
+
+            {receiptURL && 
+                <div>        
+                    <img src={receiptURL as string} alt="receipt" />
+                </div>
+            }
         </div>
-        
     );
 }
 

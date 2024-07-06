c54b96fbf7cb81b27e65ba76bdf152e287b42a2d
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2024-07-06 16:36:14 +0300

    Add typization and useEffect to AddTransaction.tsx to handle the retrieval of the user's transactions from database

--- expense-tracker-project/src/components/AddTransaction/AddTransaction.tsx
+++ expense-tracker-project/src/components/AddTransaction/AddTransaction.tsx
@@ -1,13 +1,84 @@
+import { useContext, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCalendarDays, faTags, faCalculator, faList, faCreditCard } from '@fortawesome/free-solid-svg-icons';
+import { addTransaction } from '../../service/database-service';
 import UploadReceipt from '../UploadReceipt/UploadReceipt';
+import AuthContext from '../../context/AuthContext';
 import './AddTransaction.css';
 
 const AddTransaction = () => {
+    const { isLoggedIn } = useContext(AuthContext);
+    const [loading, setLoading] = useState<boolean>(false);
+    const [salesReceipt, setSalesReceipt] = useState<string | null>(null);
+    interface NewTransaction {
+        date: string;
+        name: string;
+        amount: number;
+        category: string;
+        payment: string;
+        receipt: string;
+        user: string;
+    }
+    const [transaction, setTransaction] = useState<NewTransaction | null>(null);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const addHandler = async () => {
+            if (transaction === null) {
+                return;
+            }
+            setLoading(true);
+            await addTransaction(transaction);
+            setLoading(false);
+            navigate('/transactions');
+        }
+
+        if (transaction) addHandler();
+    }, [transaction]);
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const receiptURL = salesReceipt ? salesReceipt : 'none';
+
+        const target = event.target as typeof event.target & {
+            'expense-date': { value: string };
+            'expense-name': { value: string };
+            'expense-amount': { value: number };
+            'expense-category': { value: string };
+            'expense-payment': { value: string };
+        };
+
+        const expenseDate = target['expense-date'].value;
+        const expenseName = target['expense-name'].value;
+        const expenseAmount = target['expense-amount'].value;
+        const expenseCategory = target['expense-category'].value;
+        const expensePayment = target['expense-payment'].value; 
+
+        const expenseDetails = {
+            date: expenseDate,
+            name: expenseName,
+            amount: expenseAmount,
+            category: expenseCategory,
+            payment: expensePayment,
+            receipt: receiptURL,
+            user: isLoggedIn.user
+        };
+
+        setTransaction(expenseDetails);
+    }
+
+    if (loading) {
+        return (
+            <div className='spinnerContainer'>
+                <div className='spinner'></div>
+            </div>
+        )
+    }
 
     return (
         <>  
-            <form onSubmit={() => {}} className='expense-form'>
+            <form onSubmit={handleSubmit} className='expense-form'>
                 <h2>Expense Information</h2>
 
                 <div className='expense-form-row-1'>
@@ -41,7 +112,7 @@ const AddTransaction = () => {
                 </div>
 
                 <div className='expense-form-row-3'>
-                    <UploadReceipt />
+                    <UploadReceipt addReceipt={setSalesReceipt}/>
                 </div>
                 
                 <button type="submit" id='add-expense'>Add Expense</button>

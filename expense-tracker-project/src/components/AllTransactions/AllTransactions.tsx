5d9cf595cf8f85000b560eaba1080498ca712e19
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2024-07-06 18:45:46 +0300

    Add try-catch to both useEffect hooks in AllTransactions.tsx as well as loading/error state variables and button to delete transactions

--- expense-tracker-project/src/components/AllTransactions/AllTransactions.tsx
+++ expense-tracker-project/src/components/AllTransactions/AllTransactions.tsx
@@ -1,7 +1,7 @@
 import { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import AuthContext from "../../context/AuthContext";
-import { getTransactions } from "../../service/database-service";
+import { deleteTransaction, getTransactions } from "../../service/database-service";
 import './AllTransactions.css';
 
 const AllTransactions = () => {
@@ -17,20 +17,51 @@ const AllTransactions = () => {
         user: string;
     }
     const [transactions, setTransactions] = useState<FetchedTransaction[]>([]);
+    const [transactionToDelete, setTransactionToDelete] = useState<string>('');
+    const [loading, setLoading] = useState<boolean>(false);
+    const [error, setError] = useState<string|null>(null);
     const navigate = useNavigate();
 
     useEffect(() => {
         const fetchTransactions = async () => {
-            const transactions = await getTransactions(isLoggedIn.user);
-            console.log(transactions);
-            setTransactions(transactions);
+            try {
+                const transactions = await getTransactions(isLoggedIn.user);
+                setTransactions(transactions);
+            } catch (error: any) {
+                console.log(error.message);
+                setError(error.message);
+            }
         }
-
         fetchTransactions();
     }, []);
 
+    useEffect(() => {
+        const deleteHandler = async () => {
+            try {
+                setLoading(true);
+                await deleteTransaction(transactionToDelete);
+                setTransactions(transactions.filter(transaction => transaction.id !== transactionToDelete));
+                setTransactionToDelete('');
+                setLoading(false);
+            } catch (error: any) {
+                setError(error.message);
+                console.log(error.message);
+            }
+        }
+        if (transactionToDelete) deleteHandler();
+    }, [transactionToDelete]);
+
+    if (loading) {
+        return (
+            <div className='spinnerContainer'>
+                <div className='spinner'></div>
+            </div>
+        )
+    }
+
     return (
         <>
+            {error && <p>{error}</p>}
             <h1>All Transactions</h1>
             {transactions.length === 0 ? <p>No transactions found</p>
             : <table className="all-transactions-table">
@@ -42,6 +73,8 @@ const AllTransactions = () => {
                         <th>Date</th>
                         <th>Payment</th>
                         <th>Receipt</th>
+                        <th>Edit</th>
+                        <th>Delete</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -52,11 +85,16 @@ const AllTransactions = () => {
                             <td>{transaction.amount}</td>
                             <td>{transaction.date}</td>
                             <td>{transaction.payment}</td>
-                            <td>{
-                                transaction.receipt === 'none' 
-                                ? 'No receipt' 
-                                : <img src={transaction.receipt} alt="receipt" className="receiptImage" />
-                            }</td>
+                            <td>{transaction.receipt === 'none' 
+                                    ? 'No receipt' 
+                                    : <img src={transaction.receipt} alt="receipt" className="receiptImage" />
+                                }</td>
+                            <td>
+                                <button onClick={() => navigate(`/edit-transaction/${transaction.id}`)}>Edit</button>
+                            </td>
+                            <td>
+                                <button onClick={()=> setTransactionToDelete(transaction.id)}>Delete</button>
+                            </td>
                         </tr>
                     ))}
                 </tbody>

22e2a2eb450d8b9516a7cb98fd1e089c03a476c2
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2024-07-08 22:18:33 +0300

    Wrapt application in Authenticated component as well as include Navigation component in main App.tsx

--- expense-tracker-project/src/App.tsx
+++ expense-tracker-project/src/App.tsx
@@ -1,25 +1,28 @@
 import { useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AuthContext from './context/AuthContext.tsx';
+import Authenticated from './hoc/Authenticated.tsx';
 import Transactions from './views/Transactions.tsx';
 import AddTransaction from './components/AddTransaction/AddTransaction.tsx';
 import Login from './components/Login/Login.tsx';
+import Navigation from './components/Navigation/Navigation.tsx';
 import './App.css';
 
 function App() {
   const [authValue, setAuthValue] = useState({status: false, user: ''});
 
   return (
-    <BrowserRouter>
-      <AuthContext.Provider value={{ isLoggedIn: authValue, setLoginState: setAuthValue }}>
-        <Routes>
-          <Route path="/transactions" element={<Transactions />} />
-          <Route path="/add-transaction" element={<AddTransaction />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/edit-transaction/:id" element={<AddTransaction mode={"edit"} />} />
-        </Routes>
-      </AuthContext.Provider>
-    </BrowserRouter>
+      <BrowserRouter>
+        <AuthContext.Provider value={{ isLoggedIn: authValue, setLoginState: setAuthValue }}>
+          <Navigation />
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/transactions" element={<Authenticated><Transactions /></Authenticated>} />
+            <Route path="/add-transaction" element={<Authenticated><AddTransaction mode="new" /></Authenticated>} />
+            <Route path="/edit-transaction/:id" element={<Authenticated><AddTransaction mode="edit" /></Authenticated>} />
+          </Routes>
+        </AuthContext.Provider>
+      </BrowserRouter>
   )
 }
 

4d551b1e72654ba3d29525f6c297e62f3cff033e
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2024-07-10 19:58:32 +0300

    Implement the logic of the StickyTable.tsx component

--- expense-tracker-project/src/components/AllTransactions/AllTransactions.tsx
+++ expense-tracker-project/src/components/AllTransactions/AllTransactions.tsx
@@ -2,25 +2,27 @@ import { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import AuthContext from "../../context/AuthContext";
 import { deleteTransaction, getTransactions } from "../../service/database-service";
+import StickyTable from "./StickyTable";
 import './AllTransactions.css';
+
+interface FetchedTransaction {
+    id: string;
+    amount: number;
+    category: string;
+    date: string;
+    name: string;
+    payment: string;
+    receipt: string;
+    user: string;
+}
 
 const AllTransactions = () => {
     const { isLoggedIn } = useContext(AuthContext);
-    interface FetchedTransaction {
-        id: string;
-        amount: number;
-        category: string;
-        date: string;
-        name: string;
-        payment: string;
-        receipt: string;
-        user: string;
-    }
+    const navigate = useNavigate();
     const [transactions, setTransactions] = useState<FetchedTransaction[]>([]);
     const [transactionToDelete, setTransactionToDelete] = useState<string>('');
     const [loading, setLoading] = useState<boolean>(false);
     const [error, setError] = useState<string|null>(null);
-    const navigate = useNavigate();
 
     useEffect(() => {
         const fetchTransactions = async () => {
@@ -62,47 +64,13 @@ const AllTransactions = () => {
     return (
         <>
             {error && <p>{error}</p>}
-            <h1>All Transactions</h1>
-            {transactions.length === 0 ? <p>No transactions found</p>
-            : <table className="all-transactions-table">
-                <thead>
-                    <tr>
-                        <th>Category</th>
-                        <th>Name</th>
-                        <th>Amount</th>
-                        <th>Date</th>
-                        <th>Payment</th>
-                        <th>Receipt</th>
-                        <th>Edit</th>
-                        <th>Delete</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {transactions.map((transaction) => (
-                        <tr key={transaction.id}>
-                            <td>{transaction.category}</td>
-                            <td>{transaction.name}</td>
-                            <td>{transaction.amount}</td>
-                            <td>{transaction.date}</td>
-                            <td>{transaction.payment}</td>
-                            <td>{transaction.receipt === 'none' 
-                                    ? 'No receipt' 
-                                    : <img src={transaction.receipt} alt="receipt" className="receiptImage" />
-                                }</td>
-                            <td>
-                                <button onClick={() => navigate(`/edit-transaction/${transaction.id}`)}>Edit</button>
-                            </td>
-                            <td>
-                                <button onClick={()=> setTransactionToDelete(transaction.id)}>Delete</button>
-                            </td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
+            <h2>My Transactions</h2>
+            {transactions.length === 0 ? 
+                <p>No transactions found</p> : <StickyTable transactions={transactions} />
             }
             <br />
             <br />
-            <button onClick={() => navigate('/add-transaction')}>Add new transaction</button>
+            <button onClick={() => navigate('/add-transaction')}>Add transaction</button>
         </>
     );
 }

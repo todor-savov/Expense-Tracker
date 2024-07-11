e6a43ea4a3a18edd2a9cc6905ee127fbb8f59b20
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2024-07-11 20:04:56 +0300

    Add higher order component Authenticated to wrap all components that should be rendered for logged in users only

---
+++ expense-tracker-project/src/hoc/Authenticated.tsx
@@ -0,0 +1,31 @@
+import { useContext, useEffect, useState, ReactNode } from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+import { auth } from '../config/firebase-config.js';
+import AuthContext from '../context/AuthContext.js';
+
+const Authenticated = ({ children }: { children: ReactNode }) => {
+    const { isLoggedIn, setLoginState } = useContext(AuthContext);
+    const [loading, setLoading] = useState(true);
+    const location = useLocation();
+
+    useEffect(() => {
+        auth.onAuthStateChanged(currentUser => {
+            if (currentUser) setLoginState({ status: true, user: currentUser.email || '' });
+            setLoading(false);
+        });
+    }, []);
+
+    if (loading) {
+        return <div className='spinnerContainer'>
+                    <div className='spinner'></div>
+                </div>
+    }
+    
+    if(!isLoggedIn.user) {
+        return <Navigate replace to="/login" state={{ from: location }}/>
+    }
+
+    return <> {children} </>
+}
+
+export default Authenticated;

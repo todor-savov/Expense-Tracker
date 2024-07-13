d1242cbec6d093b6fa63eb6406803343e53d06d5
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2024-07-13 22:52:42 +0300

    Add VisuallyHiddenInput component in utils.ts to use it in UploadReceipt.tsx component when uploading sales receipts

--- expense-tracker-project/src/common/utils.ts
+++ expense-tracker-project/src/common/utils.ts
@@ -1,3 +1,5 @@
+import { styled } from "@mui/material";
+
 export const getCategoryIcon = (category: string): string => {
         switch (category) {
             case 'Food':
@@ -17,4 +19,16 @@ export const getCategoryIcon = (category: string): string => {
             default:
                 return '💰';
         }
-    }
+}
+
+export const VisuallyHiddenInput = styled('input')({
+    clip: 'rect(0 0 0 0)',
+    clipPath: 'inset(50%)',
+    height: 1,
+    overflow: 'hidden',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    whiteSpace: 'nowrap',
+    width: 1,
+});

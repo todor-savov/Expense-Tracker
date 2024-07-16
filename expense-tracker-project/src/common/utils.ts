b65f3e2fc4559067e99bc6a7c9e0ae7af3f04458
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2024-07-16 23:31:52 +0300

    Add getMonthAsNumber() function to utils.ts to transform string input from user to number when they are using a date filter

--- expense-tracker-project/src/common/utils.ts
+++ expense-tracker-project/src/common/utils.ts
@@ -32,3 +32,20 @@ export const VisuallyHiddenInput = styled('input')({
     whiteSpace: 'nowrap',
     width: 1,
 });
+
+export const getMonthAsNumber = (month: string): string => {
+    if ('january'.includes(month)) return '01';
+    if ('february'.includes(month)) return '02';
+    if ('march'.includes(month)) return '03';
+    if ('april'.includes(month)) return '04';
+    if ('may'.includes(month)) return '05';
+    if ('june'.includes(month)) return '06';
+    if ('july'.includes(month)) return '07';
+    if ('august'.includes(month)) return '08';
+    if ('september'.includes(month)) return '09';
+    if ('october'.includes(month)) return '10';
+    if ('november'.includes(month)) return '11';
+    if ('december'.includes(month)) return '12';
+    return 'Invalid month';
+}
+       

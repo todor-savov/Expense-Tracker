7603ecae26229fa245868ffa5ef9fb62622995ad
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2024-07-25 21:56:46 +0300

    Modify Navigation.tsx component so it can be called from Header.tsx with props passed to it from the latter instead

--- expense-tracker-project/src/components/Navigation/Navigation.tsx
+++ expense-tracker-project/src/components/Navigation/Navigation.tsx
@@ -1,10 +1,8 @@
-import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChartLine, faHouseUser, faLayerGroup, faMoneyBill1Wave, faScaleUnbalancedFlip } from '@fortawesome/free-solid-svg-icons';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
-import Button from '@mui/material/Button';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
@@ -12,17 +10,16 @@ import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import './Navigation.css';
 
-const Navigation = () => {
-  const [open, setOpen] = useState(false);
+interface NavigationProps {
+    setIsNavigationOpen: (isNavigationOpen: boolean) => void;
+}
+
+const Navigation: React.FC<NavigationProps> = ({ setIsNavigationOpen }) => {
   const navigate = useNavigate();
 
-  const toggleDrawer = (newOpen: boolean) => () => {
-    setOpen(newOpen);
-  };
-
   const DrawerList = (
-    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)} className="drawer-list">
-        {<List>
+    <Box sx={{ width: 250 }} role="presentation" onClick={() => setIsNavigationOpen(false)} className="drawer-list">
+        <List>
             <ListItem key={'Home'} disablePadding onClick={() => navigate('/')}>
                 <ListItemButton>
                     <ListItemIcon><FontAwesomeIcon icon={faHouseUser} size="2xl" style={{color: "#FFD43B",}} /></ListItemIcon>
@@ -57,16 +54,12 @@ const Navigation = () => {
                     <ListItemText primary={'Budget & Goals'} />
                 </ListItemButton>
             </ListItem>
-        </List>}
-    </Box>
-  );
+        </List>
+    </Box>);
 
   return (
     <div>
-      <Button onClick={toggleDrawer(true)} className="navigation-button"> 
-        <img width="78" height="78" src="https://img.icons8.com/external-flat-papa-vector/78/external-Left-Double-Arrow-interface-flat-papa-vector.png" alt="external-Left-Double-Arrow-interface-flat-papa-vector"/>
-      </Button>
-      <Drawer open={open} onClose={toggleDrawer(false)}>
+      <Drawer open={true} onClose={() => setIsNavigationOpen(false)}>
         {DrawerList}
       </Drawer>
     </div>

63fd72f9d5196ccd9f3a53dbcf3845f9a682dc4b
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2024-07-25 22:01:50 +0300

    Remove div elements from App.tsx for being redundant and call Header.tsx component in each route separately

--- expense-tracker-project/src/App.tsx
+++ expense-tracker-project/src/App.tsx
@@ -1,14 +1,14 @@
+import './App.css';
 import { useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AuthContext from './context/AuthContext.tsx';
 import Authenticated from './hoc/Authenticated.tsx';
-import Navigation from './components/Navigation/Navigation.tsx';
 import Home from './views/Home/Home.tsx';
 import Login from './components/Login/Login.tsx';
 import Transactions from './views/Transactions.tsx';
 import AddTransaction from './components/AddTransaction/AddTransaction.tsx';
 import Overview from './components/Overview/Overview.tsx';
-import './App.css';
+import Header from './components/Header/Header.tsx';
 
 function App() {
   const [authValue, setAuthValue] = useState({status: false, user: ''});
@@ -16,19 +16,14 @@ function App() {
   return (
       <BrowserRouter>
         <AuthContext.Provider value={{ isLoggedIn: authValue, setLoginState: setAuthValue }}>
-          <div className="app-container">
-              <Navigation />
-              <div className="content">
                 <Routes>
-                  <Route path="/" element={<Authenticated><Home /></Authenticated>} />
+                  <Route path="/" element={<Authenticated><Header from={"Home (Latest Transactions)"} /> <Home /></Authenticated>} />
                   <Route path="/login" element={<Login />} />
-                  <Route path="/transactions" element={<Authenticated><Transactions /></Authenticated>} />
-                  <Route path="/add-transaction" element={<Authenticated><AddTransaction mode="new" /></Authenticated>} />
-                  <Route path="/edit-transaction/:id" element={<Authenticated><AddTransaction mode="edit" /></Authenticated>} />
-                  <Route path="/overview" element={<Authenticated><Overview /></Authenticated>} />
+                  <Route path="/transactions" element={<Authenticated><Header from={"Transactions"} /> <Transactions /></Authenticated>} />
+                  <Route path="/add-transaction" element={<Authenticated><Header from={"Add Transaction"} /> <AddTransaction mode="new" /></Authenticated>} />
+                  <Route path="/edit-transaction/:id" element={<Authenticated><Header from={"Edit Transaction"} /> <AddTransaction mode="edit" /></Authenticated>} />
+                  <Route path="/overview" element={<Authenticated><Header from={"Overview"} /> <Overview /></Authenticated>} />
                 </Routes>
-              </div>
-          </div>
         </AuthContext.Provider>
       </BrowserRouter>
   )

d897e8c8bee22045777225108440f19e8699ce33
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2024-07-25 16:52:55 +0300

    Wrap the application content in a div element with the Navigation.tsx componen t and the components loaded by the respective routes being its children - in App.tsx and App.css

--- expense-tracker-project/src/App.tsx
+++ expense-tracker-project/src/App.tsx
@@ -2,12 +2,13 @@ import { useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AuthContext from './context/AuthContext.tsx';
 import Authenticated from './hoc/Authenticated.tsx';
+import Navigation from './components/Navigation/Navigation.tsx';
+import Home from './views/Home/Home.tsx';
+import Login from './components/Login/Login.tsx';
 import Transactions from './views/Transactions.tsx';
 import AddTransaction from './components/AddTransaction/AddTransaction.tsx';
-import Login from './components/Login/Login.tsx';
-import Navigation from './components/Navigation/Navigation.tsx';
-import './App.css';
 import Overview from './components/Overview/Overview.tsx';
+import './App.css';
 
 function App() {
   const [authValue, setAuthValue] = useState({status: false, user: ''});
@@ -15,14 +16,19 @@ function App() {
   return (
       <BrowserRouter>
         <AuthContext.Provider value={{ isLoggedIn: authValue, setLoginState: setAuthValue }}>
-          <Navigation />
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/transactions" element={<Authenticated><Transactions /></Authenticated>} />
-            <Route path="/add-transaction" element={<Authenticated><AddTransaction mode="new" /></Authenticated>} />
-            <Route path="/edit-transaction/:id" element={<Authenticated><AddTransaction mode="edit" /></Authenticated>} />
-            <Route path="/overview" element={<Authenticated><Overview /></Authenticated>} />
-          </Routes>
+          <div className="app-container">
+              <Navigation />
+              <div className="content">
+                <Routes>
+                  <Route path="/" element={<Authenticated><Home /></Authenticated>} />
+                  <Route path="/login" element={<Login />} />
+                  <Route path="/transactions" element={<Authenticated><Transactions /></Authenticated>} />
+                  <Route path="/add-transaction" element={<Authenticated><AddTransaction mode="new" /></Authenticated>} />
+                  <Route path="/edit-transaction/:id" element={<Authenticated><AddTransaction mode="edit" /></Authenticated>} />
+                  <Route path="/overview" element={<Authenticated><Overview /></Authenticated>} />
+                </Routes>
+              </div>
+          </div>
         </AuthContext.Provider>
       </BrowserRouter>
   )

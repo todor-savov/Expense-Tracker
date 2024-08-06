db9cc183000e36d2f8e8cd781d60843d50560764
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2024-08-06 22:25:10 +0300

    Add console.log(value) in the return statement of setLoginState function key in AuthContext.tsx

--- expense-tracker-project/src/context/AuthContext.tsx
+++ expense-tracker-project/src/context/AuthContext.tsx
@@ -2,7 +2,7 @@ import { createContext } from "react";
 
 const AuthContext = createContext({  
     isLoggedIn: {status: false, user: ''},
-    setLoginState: (value: {status: boolean, user: string}) => {}
+    setLoginState: (value: {status: boolean, user: string}) => {console.log(value)}
 });
     
 export default AuthContext;

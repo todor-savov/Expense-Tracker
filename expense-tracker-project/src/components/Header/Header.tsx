2a11d751de0267dfe68d05079f75fb1593dc0509
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2024-08-08 20:20:38 +0300

    Remove the Login/Logout switch from Header.tsx

--- expense-tracker-project/src/components/Header/Header.tsx
+++ expense-tracker-project/src/components/Header/Header.tsx
@@ -7,9 +7,6 @@ import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import AccountCircle from '@mui/icons-material/AccountCircle';
-import Switch from '@mui/material/Switch';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormGroup from '@mui/material/FormGroup';
 import MenuItem from '@mui/material/MenuItem';
 import Menu from '@mui/material/Menu';
 import Navigation from '../Navigation/Navigation';
@@ -23,10 +20,6 @@ const Header: React.FC<HeaderProps> = ({ from }) => {
   const [anchorEl, setAnchorEl] = useState<HTMLElement|null>(null);
   const [isNavigationOpen, setIsNavigationOpen] = useState<boolean>(false);
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setAuth(event.target.checked);
-  };
-
   const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget);
   };
@@ -38,10 +31,6 @@ const Header: React.FC<HeaderProps> = ({ from }) => {
   return (
     isNavigationOpen ? <Navigation setIsNavigationOpen={setIsNavigationOpen} /> 
     : <Box sx={{ flexGrow: 1 }}>
-        <FormGroup>
-            <FormControlLabel control={<Switch checked={auth} onChange={handleChange} aria-label="login switch" />}
-              label={auth ? 'Logout' : 'Login'} />
-        </FormGroup>
       <AppBar position="static">
         <Toolbar>
           <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }} onClick={() => setIsNavigationOpen(true)}>

106e73b7d11eedbca091bce5159207fb5417c42e
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2024-08-15 23:48:02 +0300

    Create searchIcons() function in icons-service.ts to handle searching for icons from IconFinder API

---
+++ expense-tracker-project/src/service/icons-service.ts
@@ -0,0 +1,16 @@
+export const searchIcons = async (searchTerm: string) => {
+    try {
+        const response = await fetch(`/api/v4/icons/search?query=${searchTerm}&count=10`, {
+            method: 'GET',
+            headers: {
+                accept: 'application/json',
+                Authorization: `Bearer ${import.meta.env.VITE_ICON_FINDER_API_KEY}`
+            }
+        });
+        return response.json();
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+

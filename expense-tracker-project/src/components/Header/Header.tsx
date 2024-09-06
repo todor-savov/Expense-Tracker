9270c624cce007df4686f787a4557028bc95d47f
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2024-09-06 18:50:03 +0300

    Implement Login and Logout buttons in the Header.tsx component

--- expense-tracker-project/src/components/Header/Header.tsx
+++ expense-tracker-project/src/components/Header/Header.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -10,15 +10,20 @@ import AccountCircle from '@mui/icons-material/AccountCircle';
 import MenuItem from '@mui/material/MenuItem';
 import Menu from '@mui/material/Menu';
 import Navigation from '../Navigation/Navigation';
+import AuthContext from '../../context/AuthContext';
+import { LoginOutlined } from '@mui/icons-material';
+import { signOutUser } from '../../service/authentication-service';
+import { useNavigate } from 'react-router-dom';
 
 interface HeaderProps {
     from: string;
 }
 
 const Header: React.FC<HeaderProps> = ({ from }) => {
-  const [auth, setAuth] = useState(true);
+  const { isLoggedIn, setLoginState } = useContext(AuthContext);
   const [anchorEl, setAnchorEl] = useState<HTMLElement|null>(null);
   const [isNavigationOpen, setIsNavigationOpen] = useState<boolean>(false);
+  const navigate = useNavigate();
 
   const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget);
@@ -26,8 +31,20 @@ const Header: React.FC<HeaderProps> = ({ from }) => {
 
   const handleClose = () => {
     setAnchorEl(null);
-    setAuth(true);
   };
+
+  const handleLoginClick = () => {
+    navigate('/login');
+    //setAnchorEl(null);
+    //setLoginState({status: false, user: ''});
+  }
+
+  const handleLogoutClick = () => {
+    signOutUser();
+    setLoginState({status: false, user: ''});
+    setAnchorEl(null);
+    navigate('/');
+  }
 
   return (
     isNavigationOpen ? <Navigation setIsNavigationOpen={setIsNavigationOpen} /> 
@@ -38,7 +55,7 @@ const Header: React.FC<HeaderProps> = ({ from }) => {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}> {from} </Typography>
-          {auth && 
+          {isLoggedIn.status ? 
             <div>
               <IconButton size="large" aria-label="account of current user" aria-controls="menu-appbar" aria-haspopup="true"
                 onClick={handleMenu} color="inherit">
@@ -48,8 +65,13 @@ const Header: React.FC<HeaderProps> = ({ from }) => {
                 transformOrigin={{vertical: 'top', horizontal: 'right',}} open={Boolean(anchorEl)} onClose={handleClose}>
                 <MenuItem onClick={handleClose}>Profile</MenuItem>
                 <MenuItem onClick={handleClose}>Settings</MenuItem>
+                <MenuItem onClick={handleLogoutClick}>Logout</MenuItem>
               </Menu>
             </div>
+            : <IconButton size="large" aria-label="account of current user" aria-controls="menu-appbar" aria-haspopup="true"
+                onClick={handleLoginClick} color="inherit">
+                <LoginOutlined />
+              </IconButton>
           }
         </Toolbar>
       </AppBar>

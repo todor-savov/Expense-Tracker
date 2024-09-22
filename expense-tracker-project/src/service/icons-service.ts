a0fced237c3cad66c71b81876c9d6ed00b4d0fcb
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2024-09-23 00:09:47 +0300

    Add the query parameters when calling the proxy API (Vercel) URL in searchIcons() function (icons-service.ts) so it can convey them to the IconFinder API on its part

--- expense-tracker-project/src/service/icons-service.ts
+++ expense-tracker-project/src/service/icons-service.ts
@@ -2,8 +2,7 @@ export const searchIcons = async (searchTerm: string) => {
     try {
         const isDevelopment = process.env.NODE_ENV === 'development';
         const API_URL = isDevelopment ? `/api/v4/icons/search?query=${searchTerm}&count=20`
-                                     : `https://expense-tracker-murex-beta.vercel.app/api/icons`;
-
+                                     : `https://expense-tracker-murex-beta.vercel.app/api/icons?query=${searchTerm}&count=20`;
         const response = await fetch(API_URL);
         return response.json();
     } catch (error) {

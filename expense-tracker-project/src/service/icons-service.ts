c8c554c6b1899fd14b137520f0121c1abcec7d21
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2024-09-22 23:16:24 +0300

    Remove unnecessary headers for the GET request in searchIcons() (icons-service.ts) as they are moved to the Vercel-hosted proxy server now

--- expense-tracker-project/src/service/icons-service.ts
+++ expense-tracker-project/src/service/icons-service.ts
@@ -2,16 +2,9 @@ export const searchIcons = async (searchTerm: string) => {
     try {
         const isDevelopment = process.env.NODE_ENV === 'development';
         const API_URL = isDevelopment ? `/api/v4/icons/search?query=${searchTerm}&count=20`
-                                     : `https://api.iconfinder.com/v4/icons/search?query=${searchTerm}&count=20`;
+                                     : `https://expense-tracker-murex-beta.vercel.app/api/icons`;
 
-        const response = await fetch(API_URL, {
-            method: 'GET',
-            headers: {
-                accept: 'application/json',
-                Authorization: `Bearer TFSDEF1JlpHXluuAhZvOxRijfrEGZRE3mVm61M6ZEusHxRhVxTXDphHKp8N1baVk`
-            }
-        });
-        
+        const response = await fetch(API_URL);
         return response.json();
     } catch (error) {
         console.error(error);

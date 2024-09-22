e2ffd72dec02c92736c95ea8ac83dae450869b70
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2024-09-22 22:48:36 +0300

    Update searchIcons() function in icons-service.ts so it distinguishes automatically when the environment is production or development and use the local Vite proxy (in dev mode) or the iconfinder-proxy (in production mode)

--- expense-tracker-project/src/service/icons-service.ts
+++ expense-tracker-project/src/service/icons-service.ts
@@ -1,12 +1,17 @@
 export const searchIcons = async (searchTerm: string) => {
     try {
-        const response = await fetch(`/api/v4/icons/search?query=${searchTerm}&count=20`, {
+        const isDevelopment = process.env.NODE_ENV === 'development';
+        const API_URL = isDevelopment ? `/api/v4/icons/search?query=${searchTerm}&count=20`
+                                     : `https://api.iconfinder.com/v4/icons/search?query=${searchTerm}&count=20`;
+
+        const response = await fetch(API_URL, {
             method: 'GET',
             headers: {
                 accept: 'application/json',
-                Authorization: `Bearer ${import.meta.env.VITE_ICON_FINDER_API_KEY}`
+                Authorization: `Bearer TFSDEF1JlpHXluuAhZvOxRijfrEGZRE3mVm61M6ZEusHxRhVxTXDphHKp8N1baVk`
             }
         });
+        
         return response.json();
     } catch (error) {
         console.error(error);

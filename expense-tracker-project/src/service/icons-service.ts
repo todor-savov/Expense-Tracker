32094ca86d768635db31aae923e3ad618ea7e491
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2024-09-23 17:06:00 +0300

    Update searchIcons() function in icons-service.ts to handle both situations - fetch requests from IconFinder API through the Vite proxy (in dev mode) and through the Vercel proxy (in production mode)

--- expense-tracker-project/src/service/icons-service.ts
+++ expense-tracker-project/src/service/icons-service.ts
@@ -1,10 +1,23 @@
 export const searchIcons = async (searchTerm: string) => {
     try {
         const isDevelopment = process.env.NODE_ENV === 'development';
-        const API_URL = isDevelopment ? `/api/v4/icons/search?query=${searchTerm}&count=20`
-                                     : `https://expense-tracker-murex-beta.vercel.app/api/icons?query=${searchTerm}&count=20`;
-        const response = await fetch(API_URL);
-        return response.json();
+        const API_URL = isDevelopment ? 
+                        `/api/v4/icons/search?query=${searchTerm}&count=20` 
+                        :
+                        `https://expense-tracker-murex-beta.vercel.app/api/icons?query=${searchTerm}&count=20`;
+        if (isDevelopment) {
+            const response = await fetch(API_URL, {
+                method: 'GET',
+                headers: {
+                  'accept': 'application/json',
+                  'Authorization': `Bearer ${import.meta.env.VITE_ICON_FINDER_API_KEY}`
+                }
+            });
+            return response.json();
+        } else {
+            const response = await fetch(API_URL);
+            return response.json();
+        }
     } catch (error) {
         console.error(error);
     }

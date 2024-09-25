4e17229adc3cdb9dd36340742dc41733f2f090c5
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2024-09-25 20:40:05 +0300

    Add Badge and Popover components from Material UI to Header.tsx and separate their onClick/onClose handlers in separate functions

--- expense-tracker-project/src/components/Header/Header.tsx
+++ expense-tracker-project/src/components/Header/Header.tsx
@@ -11,10 +11,11 @@ import MenuItem from '@mui/material/MenuItem';
 import Menu from '@mui/material/Menu';
 import Navigation from '../Navigation/Navigation';
 import AuthContext from '../../context/AuthContext';
-import { LoginOutlined } from '@mui/icons-material';
+import { AccountBox, AccountCircle, Email, EmailOutlined, EmailRounded, EmailTwoTone, LoginOutlined, Logout, LogoutOutlined, LogoutRounded, MailLock, Settings, SportsBaseball, Twitter } from '@mui/icons-material';
 import { signOutUser } from '../../service/authentication-service';
 import { getUserDetails } from '../../service/database-service';
 import "./Header.css";
+import { Badge, Button, Icon, Popover } from '@mui/material';
 
 interface HeaderProps {
     from: string;
@@ -36,6 +37,8 @@ const Header = ({ from, isUserChanged }: HeaderProps) => {
   const { isLoggedIn, setLoginState } = useContext(AuthContext);
   const [anchorEl, setAnchorEl] = useState<HTMLElement|null>(null);
   const [isNavigationOpen, setIsNavigationOpen] = useState<boolean>(false);
+  const [isBadgeOpen, setIsBadgeOpen] = useState<boolean>(false);
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
   const [currentUser, setCurrentUser] = useState<UserDetails|null>(null);
   const navigate = useNavigate();
 
@@ -47,30 +50,38 @@ const Header = ({ from, isUserChanged }: HeaderProps) => {
       fetchUserDetails();
   }, [isUserChanged]);
 
-  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+  const handleMenuClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     setAnchorEl(event.currentTarget);
+    setIsMenuOpen(true);
   };
 
-  const handleClose = () => {
+  const handleMenuClose = () => {
     setAnchorEl(null);
+    setIsMenuOpen(false);
   };
+
+  const handleBadgeClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+    setIsBadgeOpen(true);
+  };
+
+  const handleBadgeClose = () => { 
+    setAnchorEl(null);
+    setIsBadgeOpen(false);
+  }
 
   const handleLoginClick = () => {
     navigate('/login');
-    //setAnchorEl(null);
-    //setLoginState({status: false, user: ''});
   }
 
   const handleLogoutClick = () => {
     signOutUser();
     setLoginState({status: false, user: ''});
-    setAnchorEl(null);
     navigate('/');
   }
 
   const handleProfileClick = () => {
     navigate('/profile');
-    setAnchorEl(null);
   }
 
   return (
@@ -81,21 +92,31 @@ const Header = ({ from, isUserChanged }: HeaderProps) => {
           <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }} onClick={() => setIsNavigationOpen(true)}>
             <MenuIcon />
           </IconButton>
+          <Typography component="span"> Welcome {currentUser?.firstName}!  </Typography>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}> {from} </Typography>
           {isLoggedIn.status ? 
             <div>
               <span>
-                <Typography component="span">Welcome {currentUser?.firstName} {currentUser?.lastName}!  </Typography>
+                <Button aria-describedby='simple-popover' aria-controls="simple-popover" variant="contained" onClick={handleBadgeClick}>
+                  <Badge color="secondary" badgeContent={3} invisible={false}> <EmailTwoTone /> </Badge>
+                </Button>
+
                 <IconButton size="large" aria-label="account of current user" aria-controls="menu-appbar" aria-haspopup="true"
-                  onClick={handleMenu} color="inherit">
+                  onClick={handleMenuClick} color="inherit">
                     <img id="profile-photo" src={currentUser?.photo} alt="profile" />
                 </IconButton>
               </span>
+              <Popover id='simple-popover' open={isBadgeOpen} anchorEl={anchorEl} onClose={handleBadgeClose}
+                  anchorOrigin={{ vertical: 'bottom', horizontal: 'right',}}
+                  transformOrigin={{ vertical: 'top', horizontal: 'right',}}
+              >
+                  <Typography sx={{ p: 2 }}>You have not logged in any transactions in the last 3 days.</Typography>
+              </Popover>
               <Menu id="menu-appbar" anchorEl={anchorEl} anchorOrigin={{vertical: 'top', horizontal: 'right',}} keepMounted
-                transformOrigin={{vertical: 'top', horizontal: 'right',}} open={Boolean(anchorEl)} onClose={handleClose}>
-                <MenuItem onClick={handleProfileClick}>Profile</MenuItem>
-                <MenuItem onClick={handleClose}>Settings</MenuItem>
-                <MenuItem onClick={handleLogoutClick}>Logout</MenuItem>
+                transformOrigin={{vertical: 'top', horizontal: 'right',}} open={isMenuOpen} onClose={handleMenuClose}>
+                <MenuItem onClick={handleProfileClick}><AccountCircle /> Profile </MenuItem>
+                <MenuItem onClick={handleMenuClose}><Settings /> Settings </MenuItem>
+                <MenuItem onClick={handleLogoutClick}><Logout /> Logout </MenuItem>
               </Menu>
             </div>
             : <IconButton size="large" aria-label="account of current user" aria-controls="menu-appbar" aria-haspopup="true"

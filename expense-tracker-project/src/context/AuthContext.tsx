d8faef4f9628535b0ca9f66ec078743f6c1543ab
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2024-10-15 18:22:05 +0300

    Update the AuthContext.tsx (the app's context) to add a settings state and a function that updates it (this way user settings will be accessible in the entire app)

--- expense-tracker-project/src/context/AuthContext.tsx
+++ expense-tracker-project/src/context/AuthContext.tsx
@@ -1,8 +1,23 @@
 import { createContext } from "react";
 
-const AuthContext = createContext({  
+interface UserSettings { 
+    activityNotifications: string;
+    budgetNotifications: string;
+    currency: string;
+}
+
+interface AuthContextType {
+    isLoggedIn: { status: boolean; user: string };
+    setLoginState: (value: { status: boolean; user: string }) => void;
+    settings: UserSettings | null;
+    setSettings: (settings: UserSettings | null) => void;
+  }
+
+const AuthContext = createContext<AuthContextType>({  
     isLoggedIn: {status: false, user: ''},
-    setLoginState: (value: {status: boolean, user: string}) => {console.log(value)}
+    setLoginState: (value: {status: boolean, user: string}) => {console.log(value)},
+    settings: null,
+    setSettings: () => {},
 });
     
 export default AuthContext;

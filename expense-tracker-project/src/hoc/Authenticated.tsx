c9023aea1f5683670df643761539e2e3d3976fd3
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2024-10-16 14:21:36 +0300

    Refactor the useEffect hook in Authenticated.tsx hoc to fetch the user settings from the database after confirming they are logged in by calling the Firebase authentication service before that

--- expense-tracker-project/src/hoc/Authenticated.tsx
+++ expense-tracker-project/src/hoc/Authenticated.tsx
@@ -2,17 +2,32 @@ import { useContext, useEffect, useState, ReactNode } from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 import { auth } from '../config/firebase-config.js';
 import AuthContext from '../context/AuthContext.js';
+import { getUserSettings } from '../service/database-service.js';
+
+interface UserSettings { 
+    activityNotifications: string;
+    budgetNotifications: string;
+    currency: string;
+}
 
 const Authenticated = ({ children }: { children: ReactNode }) => {
-    const { isLoggedIn, setLoginState } = useContext(AuthContext);
+    const { isLoggedIn, setLoginState, setSettings } = useContext(AuthContext);
     const [loading, setLoading] = useState<boolean>(true);
     const location = useLocation();
 
     useEffect(() => {
-        auth.onAuthStateChanged(currentUser => {
-            if (currentUser) setLoginState({ status: true, user: currentUser.email || '' });
+        auth.onAuthStateChanged(async currentUser => {
+            if (currentUser) {                       
+                try {                    
+                    setLoginState({ status: true, user: currentUser.email || '' });
+                    const userSettings = await getUserSettings(currentUser.email as string);
+                    setSettings(userSettings as UserSettings);
+                } catch (error) {
+                    console.log(error);
+                }
+            }
             setLoading(false);
-        });
+        });       
     }, []);
 
     if (loading) {

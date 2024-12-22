f241c20334b4b4350cff556f13204161807eb193
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2024-12-22 21:41:38 +0200

    Refactor the loading and error handling in Settings.tsx to user Snackbar component instead

--- expense-tracker-project/src/components/Settings/Settings.tsx
+++ expense-tracker-project/src/components/Settings/Settings.tsx
@@ -1,8 +1,6 @@
 import { useContext, useEffect, useState } from "react";
-import { Box, CircularProgress, FormControl, FormControlLabel, FormGroup, InputLabel, MenuItem, Select, 
-        SelectChangeEvent, Stack, Switch, Typography } from "@mui/material";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircleCheck, faCircleXmark } from "@fortawesome/free-solid-svg-icons";
+import { Alert, Box, CircularProgress, FormControl, FormControlLabel, FormGroup, InputLabel, MenuItem, Select, 
+        SelectChangeEvent, Snackbar, Stack, Switch, Typography } from "@mui/material";
 import AuthContext from "../../context/AuthContext";
 import { updateUserSettings } from "../../service/database-service";
 import './Settings.css';
@@ -15,28 +13,33 @@ interface UserSettings {
 
 const Settings = () => {
     const { isLoggedIn, settings, setSettings } = useContext(AuthContext);
-    const [currency, setCurrency] = useState<string>(settings?.currency || 'BGN');
+    const [userSettings, setUserSettings] = useState<UserSettings|null>(null);
     const [activityNotifications, setActivityNotifications] = useState<boolean>(settings?.activityNotifications === 'enabled');
     const [budgetNotifications, setBudgetNotifications] = useState<boolean>(settings?.budgetNotifications === 'enabled');
-    const [userSettings, setUserSettings] = useState<UserSettings|null>(null);
+    const [currency, setCurrency] = useState<string>(settings?.currency || 'BGN');
     const [isFormSubmitted, setIsFormSubmitted] = useState<boolean>(false);
-    const [loading, setLoading] = useState<boolean>(false);
+    const [successMessage, setSuccessMessage] = useState<string|null>(null);
     const [error, setError] = useState<string|null>(null);
+    const [loading, setLoading] = useState<boolean>(false);
+    const [openSnackbar, setOpenSnackbar] = useState<boolean>(false);
 
     useEffect(() => {
         const updateSettings = async () => {
             try {
+                setError(null);
+                setSuccessMessage(null);
                 setLoading(true);
                 const response = await updateUserSettings(isLoggedIn.user, userSettings as UserSettings);
-                if (response) throw new Error(response);
-                setLoading(false);
-                setError(null);
+                if (response) throw new Error('Settings update has failed.');
+                setSuccessMessage('Settings have been updated successfully.');  
                 setSettings(userSettings);
             } catch (error: any) {
-                setLoading(false);
                 setError(error.message);
                 console.log(error.message);                                
-            } 
+            } finally {
+                setLoading(false);
+                setOpenSnackbar(true);
+            }
         }
         if (userSettings) updateSettings();
     }, [userSettings]);
@@ -54,34 +57,30 @@ const Settings = () => {
         setCurrency(currency);
     }
 
+    const handleSnackbarClose = () => {
+        setOpenSnackbar(false);
+    }
+
     return (
             <Box component={"form"} onSubmit={handleSubmit} className="settings-container">
-                <Typography variant="h5" sx={{ marginBottom: '20px', fontWeight: 'bold' }}>Choose your preferred settings:</Typography>
+                <Typography variant="h5" id='settings-header'>Choose your preferred settings:</Typography>
                 <FormGroup className="form-group">
                     <FormControlLabel control={
-                        <Switch 
-                            checked={activityNotifications}
-                            onChange={(e) => setActivityNotifications(e.target.checked)} 
-                            sx={{ m: 1 }} 
-                        />} 
+                        <Switch checked={activityNotifications} onChange={(e) => setActivityNotifications(e.target.checked)} className="switch" />
+                        } 
                         label="Enable activity notifications"
                     />
 
                     <FormControlLabel control={
-                        <Switch 
-                            checked={budgetNotifications}
-                            onChange={(e) => setBudgetNotifications(e.target.checked)} 
-                            sx={{ m: 1 }}
-                        />}
+                        <Switch checked={budgetNotifications} onChange={(e) => setBudgetNotifications(e.target.checked)} className="switch" />
+                        }
                         label="Enable budget notifications" 
                     />
 
                     <FormControl>
                         <InputLabel id="demo-simple-select-label">Currency</InputLabel>
-                        <Select labelId="demo-simple-select-label" id="demo-simple-select" 
-                            sx={{ width: '100%', m: 1 }} label="Currency" value={currency}
-                            onChange={handleCurrencyChange} 
-                        >
+                        <Select labelId="demo-simple-select-label" id="currency-select" label="Currency" 
+                            value={currency} onChange={handleCurrencyChange}>
                             <MenuItem value={"BGN"}>BGN</MenuItem>
                             <MenuItem value={"EUR"}>EUR</MenuItem>
                             <MenuItem value={"USD"}>USD</MenuItem>
@@ -89,28 +88,23 @@ const Settings = () => {
                     </FormControl>
                 </FormGroup>
 
-                {loading ?
-                    <Stack sx={{ color: 'grey.500' }} spacing={2} direction="row">
-                        <CircularProgress color="success" />
-                    </Stack>
-                    : (isFormSubmitted ? 
-                        (error ? 
-                            <>
-                                <button className="update-settings-button" type="submit">Update</button>
-                                <Typography variant="body2" align="center" className="feedback-message error-message">
-                                    <FontAwesomeIcon icon={faCircleXmark} size="2xl" />      
-                                    Settings update has failed.                                   
-                                </Typography>
-                            </> 
-                            :
-                                <Typography variant="body2" align="center" className="feedback-message success-message">
-                                    <FontAwesomeIcon icon={faCircleCheck} size="2xl" />
-                                    Settings update has been successful.
-                                </Typography>
-                        )
-                        : <button className="update-settings-button" type="submit">Update</button>
-                        )                    
+                {isFormSubmitted &&
+                    (loading ? 
+                        <Stack sx={{ color: 'grey.500' }} spacing={2} direction="row" id='spinning-circle'>
+                            <CircularProgress color="success" size='3rem' />
+                        </Stack>
+                        : 
+                        <Snackbar open={openSnackbar} autoHideDuration={6000} onClose={handleSnackbarClose}
+                            anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }} sx={{ marginBottom: 8 }}
+                        >
+                            <Alert onClose={handleSnackbarClose} severity={error ? 'error' : 'success'} variant="filled">
+                                {error ? error : successMessage}
+                            </Alert>
+                        </Snackbar>
+                    )
                 }
+                                                                
+                {!loading && <button className="update-settings-button" type="submit">Update</button>}                        
             </Box>
     );
 }

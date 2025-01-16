2b1440284ed9811156ab5e968956d81812596f52
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2025-01-16 19:14:03 +0200

    Add typization for the getExchangeRates() function in exchange-rate-service.ts

--- expense-tracker-project/src/service/exchange-rate-service.ts
+++ expense-tracker-project/src/service/exchange-rate-service.ts
@@ -1,5 +1,9 @@
-export const getExchangeRates = async (baseCurrency: string) => {  
-    const response = await fetch(`https://v6.exchangerate-api.com/v6/${import.meta.env.VITE_EXCHANGE_RATE_API_KEY}/latest/${baseCurrency}`); 
-    const data = await response.json();
-    return data.conversion_rates;
+export const getExchangeRates = async (baseCurrency: string): Promise<Record<string, number>|undefined> => {  
+    try {        
+        const response = await fetch(`https://v6.exchangerate-api.com/v6/${import.meta.env.VITE_EXCHANGE_RATE_API_KEY}/latest/${baseCurrency}`); 
+        const data = await response.json();
+        return data.conversion_rates;
+    } catch (error: any) {
+        console.log(error.message);
+    }            
 }

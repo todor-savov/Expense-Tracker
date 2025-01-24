457b2b047af06a2ca1c022019d9427f8a032f978
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2025-01-24 18:56:02 +0200

    Refactor the uploadUserPhoto function in storage-service.ts to include undefined as possible output (when error is encountered during file upload to storage service)

--- expense-tracker-project/src/service/storage-service.ts
+++ expense-tracker-project/src/service/storage-service.ts
@@ -9,14 +9,20 @@ export const uploadFile = async (file: File): Promise<string|undefined> => {
         if (!status) throw new Error("Upload failed");
         return await getDownloadURL(storageRef);
     } catch (error: any) {
-        console.error(error.message);        
+        console.log(error.message);        
     }    
 };
 
-export const uploadUserPhoto = async (file: File): Promise<string> => {
-    const storageRef = ref(storage, `users/${file.name}`);
-    await uploadBytes(storageRef, file);
-    return await getDownloadURL(storageRef);
+export const uploadUserPhoto = async (file: File): Promise<string|undefined> => {
+    try {
+        const storageRef = ref(storage, `users/${file.name}`);
+        if (!storageRef) throw new Error("Storage reference is not found");
+        const status = await uploadBytes(storageRef, file);
+        if (!status) throw new Error("Upload failed");
+        return await getDownloadURL(storageRef);
+    } catch (error: any) {
+        console.log(error.message);        
+    }
 }
 
 

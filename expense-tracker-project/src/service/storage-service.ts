a85285dfb335d4f72c4e20aafe0d7b4ec7253020
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2025-01-30 22:13:08 +0200

    Revise and refactor the output type of all functions in storage-service.ts and the components that call them

--- expense-tracker-project/src/service/storage-service.ts
+++ expense-tracker-project/src/service/storage-service.ts
@@ -2,26 +2,22 @@ import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import { storage } from "../config/firebase-config.ts";
 
 export const uploadFile = async (file: File): Promise<string|undefined> => {  
-    try {        
+    try {
         const storageRef = ref(storage, `receipts/${file.name}`);
-        if (!storageRef) throw new Error("Storage reference is not found");
-        const status = await uploadBytes(storageRef, file);
-        if (!status) throw new Error("Upload failed");
+        await uploadBytes(storageRef, file);
         return await getDownloadURL(storageRef);
     } catch (error: any) {
-        console.log(error.message);        
+        console.log(error.message);
     }    
 };
 
 export const uploadUserPhoto = async (file: File): Promise<string|undefined> => {
     try {
         const storageRef = ref(storage, `users/${file.name}`);
-        if (!storageRef) throw new Error("Storage reference is not found");
-        const status = await uploadBytes(storageRef, file);
-        if (!status) throw new Error("Upload failed");
+        await uploadBytes(storageRef, file);
         return await getDownloadURL(storageRef);
     } catch (error: any) {
-        console.log(error.message);        
+        console.log(error.message);
     }
 }
 

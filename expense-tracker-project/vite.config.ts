a519910560a7ece635976e547891a4e0dedeae44
todor-savov <todor.savov.a58@learn.telerikacademy.com>
2025-02-10 14:12:48 +0200

    Remove the proxy configuration from vite.config.ts as it is not needed anymore

--- expense-tracker-project/vite.config.ts
+++ expense-tracker-project/vite.config.ts
@@ -1,17 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { createProxyMiddleware } from 'http-proxy-middleware';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-        '/api': {
-            target: 'https://api.iconfinder.com',
-            changeOrigin: true,
-            rewrite: (path) => path.replace(/^\/api/, '')
-        }
-    }
-  }
+  plugins: [react()],  
 })
